1fe15d96844ffaf27b127168e5708ec210853371
Jared Miller <jaredmiller219@gmail.com>
2025-02-23 19:18:48 -0500

    Implement game over logic and restart functionality; update character controller to handle collisions

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -9,16 +9,21 @@ public class CharController : MonoBehaviour
     // This is the jump force that will be applied to the player
     public float jumpForce = 10;
 
+    // This is a reference to the GameStateManager script
+    public GameStateManager logic;
+
+    public bool isAlive = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<GameStateManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive == true)
         {
             jump();
         }
@@ -26,5 +31,11 @@ public class CharController : MonoBehaviour
 
     void jump(){
         player.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * jumpForce;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        isAlive = false;
     }
 }
--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
@@ -7,12 +8,26 @@ public class GameStateManager : MonoBehaviour
 
     public Text scoreText;
 
+    public GameObject gameOverScreen;
+
     [ContextMenu("Increase Score")]
 
     public void AddScore(int scoreToAdd)
     {
         pointsScored = pointsScored + scoreToAdd;
-        Debug.Log("Points scored: " + pointsScored);
+        // Debug.Log("Points scored: " + pointsScored);
         scoreText.text = pointsScored.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        pointsScored = 0;
+        scoreText.text = pointsScored.ToString();
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
